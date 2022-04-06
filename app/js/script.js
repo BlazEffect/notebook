71ea079c5d2a4629a7e2ee35cd5c7ae7e26eadd3
BlazEffect <kamalinigor@gmail.com>
2022-04-06 14:24:01 +0300

    Add comment popup

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,22 @@
 window.onload = function (){
 
-    let addNoteButton = document.querySelector(".notepad-add-button .add");
+    let addNoteOpenButton = document.querySelector(".notepad-add-button .add");
+    let addNoteCloseButton = document.querySelector(".notepad-add-popup__close-button");
+    let addNoteForm = document.querySelector(".main__notepad-add-popup");
 
-    addNoteButton.addEventListener("click", function(event){
+    addNoteOpenButton.addEventListener("click", function(event){
         event.preventDefault();
 
+        if(addNoteForm.classList.contains("notepad-add-popup--close")){
+            addNoteForm.classList.remove("notepad-add-popup--close");
+            addNoteForm.classList.add("notepad-add-popup--open");
+        }
+    });
 
-    })
+    addNoteCloseButton.addEventListener("click", function(){
+        addNoteForm.classList.remove("notepad-add-popup--open");
+        addNoteForm.classList.add("notepad-add-popup--close");
+    });
 
     function sendAjax(method, url, data){
 
