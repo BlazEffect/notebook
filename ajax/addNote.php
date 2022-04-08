8a3860c8a734c674e96cd55184c9c543c04c0501
BlazEffect <prog4@it-delta.ru>
2022-04-08 07:34:59 +0300

    Deploy backend

---
+++ ajax/addNote.php
@@ -0,0 +1,16 @@
+<?php
+
+$DB = new mysqli("localhost", "root", "", "notepad");
+
+if($DB->connect_error){
+    print_r("Подключение невозможно: ".$DB->connect_error);
+}
+
+$text = htmlspecialchars($_POST["text"]);
+
+$sql = "INSERT INTO `notes` (text) VALUES ('".$text."')";
+if($DB->query($sql)){
+    print_r("Данные успешно добавлены");
+}else{
+    print_r("Ошибка: " . $DB->error);
+}
